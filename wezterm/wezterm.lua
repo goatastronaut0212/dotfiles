4fd15cbe09893290c84e4b953cb9c3bbe6fd3309
goatastronaut0212 <goatastronaut0212@outlook.com>
2025-06-27 09:34:56 +0700

    wezterm: update white theme and transparent background

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,14 +1,20 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- Spawn a fish shell in login mode
-config.default_prog = { 'fish' }
+config.default_prog = { "fish" }
 
 -- Font
-config.font = wezterm.font('Iosevka Nerd Font Mono')
+config.font = wezterm.font("Iosevka Nerd Font Mono")
+
+-- Theme
+config.color_scheme = "Catppuccin Latte"
+
+-- Background
+config.window_background_opacity = 0.64
 
 -- and finally, return the configuration to wezterm
 return config
